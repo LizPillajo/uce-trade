95f258dc5b451665d7307d6609b69ca2ca5a7a37
LizPillajo <ldpillajo@uce.edu.ec>
2026-01-18 17:45:00 -0500

    Security keys and dependencies for SMTP

--- uce-trade-frontend/src/services/supabaseClient.js
+++ uce-trade-frontend/src/services/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://zoxtcheqhlhvznjsarsg.supabase.co'; 
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpveHRjaGVxaGxodnpuanNhcnNnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njg1MTM0NzYsImV4cCI6MjA4NDA4OTQ3Nn0.yqGZto86gA5dpvWjuP9eQdB2n3uBCUsabneaa1aMcIw';
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
